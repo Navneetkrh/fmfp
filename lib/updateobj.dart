cab13fc5997c0dabb02bb7074e52e7b91d4d20fa
Navneetkrh <108091719+Navneetkrh@users.noreply.github.com>
2023-12-19 23:53:38 +0530

    Add delete functionality to UpdateObject widget

--- lib/updateobj.dart
+++ lib/updateobj.dart
@@ -1,7 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class UpdateObject extends StatelessWidget {
+class UpdateObject extends StatefulWidget {
   final DateTime time;
   final String subject;
   final String content;
@@ -13,6 +14,11 @@ class UpdateObject extends StatelessWidget {
     required this.content,
   }) : super(key: key);
 
+  @override
+  _UpdateObjectState createState() => _UpdateObjectState();
+}
+
+class _UpdateObjectState extends State<UpdateObject> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -35,18 +41,76 @@ class UpdateObject extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Text(
-            subject,
+            widget.subject,
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
           ),
           Text(
-            content,
+            widget.content,
             style: TextStyle(fontSize: 16, color: Colors.white70),
           ),
           Align(
             alignment: Alignment.bottomRight,
-            child: Text(
-              '${time.day}/${time.month}/${time.year} ${time.hour}:${time.minute}',
-              style: TextStyle(fontSize: 14, color: Colors.grey[300]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '${widget.time.day}/${widget.time.month}/${widget.time.year} ${widget.time.hour}:${widget.time.minute}',
+                  style: TextStyle(fontSize: 14, color: Colors.grey[300]),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete, color: Colors.red),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        String password = '';
+                        return AlertDialog(
+                          title: Text('Confirm Deletion'),
+                          content: TextField(
+                            obscureText: true,
+                            onChanged: (value) {
+                              password = value;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Enter password',
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Confirm'),
+                              onPressed: () async {
+                                if (password == '3600') { // Replace 'yourPassword' with the actual password
+                                  final querySnapshot = await FirebaseFirestore.instance
+                                      .collection('activity')
+                                      .where('subject', isEqualTo: widget.subject)
+                                      .where('content', isEqualTo: widget.content)
+                                      .get();
+                                  for (final doc in querySnapshot.docs) {
+                                    await doc.reference.delete();
+                                  }
+                                  Navigator.of(context).pop(); // Close the dialog
+                                  setState(() {}); // Refresh the page
+                                } else {
+                                  // Show error message
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text('Incorrect password')),
+                                  );
+                                }
+                              },
+                            ),
+                            TextButton(
+                              child: Text('Cancel'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ],
