c4e196af549496fb4d030e2b84b6e3ae59acee59
Navneetkrh <108091719+Navneetkrh@users.noreply.github.com>
2023-12-19 12:59:54 +0530

    all major ui updates completed

--- lib/sessionobj.dart
+++ lib/sessionobj.dart
@@ -16,100 +16,104 @@ class SessionObject extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(bottom: 20),
       height: 150,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            room,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 5 ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         
+          children: [
+            Text(
+              room,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-          ),
-          Text(
-            name,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              color: Colors.white,
+            Text(
+              name,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+                color: Colors.white,
+              ),
             ),
-          ),
-          GestureDetector(
-            onTap: () {
-              // Handle the tap event here. For example, navigate to the lectures page.
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => LecturesPage()),
-              // );
-            },
-            child: ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    backgroundColor: Colors.white, // Set the background color to white
-                    titleTextStyle: TextStyle(color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold), // Set the title text style to blue
-                    contentTextStyle: TextStyle(color: Colors.black, fontSize: 16), // Set the content text style to black
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Lectures'),
-                        IconButton(
-                          icon: Icon(Icons.close),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    ),
-                    content: SingleChildScrollView( // Add a SingleChildScrollView to enable scrolling when the content is too long
-                      child: Column(
-                        children: papers.map((paper) => RichText(
-                          text: TextSpan(
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15,
-                            ),
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: '${paper['title']}\n',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextSpan(text: paper['authors'] ?? ''),
-                              TextSpan(text: '\n\n'), // Add some space between each paper
-                            ],
+            GestureDetector(
+              onTap: () {
+                // Handle the tap event here. For example, navigate to the lectures page.
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => LecturesPage()),
+                // );
+              },
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      backgroundColor: Colors.white, // Set the background color to white
+                      titleTextStyle: TextStyle(color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold), // Set the title text style to blue
+                      contentTextStyle: TextStyle(color: Colors.black, fontSize: 16), // Set the content text style to black
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Lectures'),
+                          IconButton(
+                            icon: Icon(Icons.close),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
                           ),
-                        )).toList(),
+                        ],
+                      ),
+                      content: SingleChildScrollView( // Add a SingleChildScrollView to enable scrolling when the content is too long
+                        child: Column(
+                          children: papers.map((paper) => RichText(
+                            text: TextSpan(
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: '${paper['title']}\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(text: paper['authors'] ?? ''),
+                                TextSpan(text: '\n\n'), // Add some space between each paper
+                              ],
+                            ),
+                          )).toList(),
+                        ),
                       ),
                     ),
-                  ),
-                
-                );
-              },
-              child: Center(
-                child: Text(
-                  'View Lectures',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    
+                  
+                  );
+                },
+                child: Center(
+                  child: Text(
+                    'View Lectures',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      
+                    ),
                   ),
                 ),
-              ),
-              style: ElevatedButton.styleFrom(
-                elevation: 10,
-                backgroundColor: Color.fromARGB(255, 30, 32, 70),
-                minimumSize: Size(100.0, 40.0),
-                shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(20), 
-    
-      // This makes the button rounded
-    ),
+                style: ElevatedButton.styleFrom(
+                  elevation: 10,
+                  backgroundColor: Color.fromARGB(255, 30, 32, 70),
+                  minimumSize: Size(100.0, 40.0),
+                  shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20), 
+          
+        // This makes the button rounded
+          ),
+                ),
               ),
             ),
-          ),
-        ]),
+          ]),
+      ),
       decoration: BoxDecoration(
         color: Colors.indigo, // set the color to white
         borderRadius: BorderRadius.circular(20)),
