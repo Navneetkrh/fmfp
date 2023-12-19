d9db579887dcb466f909f55383da836a86d5787c
Navneetkrh <108091719+Navneetkrh@users.noreply.github.com>
2023-12-19 22:58:12 +0530

    Update UI colors and add URL launcher plugin

--- lib/about.dart
+++ lib/about.dart
@@ -7,34 +7,37 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo, // Set the background color of the page
+      backgroundColor: Colors.indigo,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(100.0),
-      
         child: AppBar(
-            elevation: 0,
+          elevation: 0,
           leading: Image.asset('assets/fmfp.png'),
           title: Text('About us'),
           backgroundColor: Colors.indigo,
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Card(
-                color: Colors.white,
+                color: Color(0xFF1E2046),
+                shape: RoundedRectangleBorder(  // Add this line
+    borderRadius: BorderRadius.circular(20.0),  // Set the border radius here
+  ),
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                  
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       InkWell(
                         child: Text(
                           'https://events.iitj.ac.in/fmfp2023/',
-                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                          style: TextStyle(fontSize: 20, color: Colors.red), // Change the color of the link to red
                         ),
                         onTap: () async {
                           if (await canLaunch('https://events.iitj.ac.in/fmfp2023/')) {
@@ -47,7 +50,7 @@ class AboutPage extends StatelessWidget {
                       SizedBox(height: 20),
                       Text(
                         'This App was Made by the FMFP Volunteer team @ IIT Jodhpur',
-                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white), // Change the color of the text to white
                       ),
                       SizedBox(height: 20),
                       Text(
@@ -55,7 +58,7 @@ class AboutPage extends StatelessWidget {
                         'This conference is organized every year since 1969 at different venues in India with the guidance of National Society for Fluid Mechanics and Fluid Power (NSFMFP). The primary aim of this conference is to provide a platform and bring together the experts around the world to share the state-of-the-art information on various fluid mechanics and fluid power related topics.\n\n'
                         'Organized by The National Society for Fluid Mechanics and Fluid Power (NSFMFP) and Department of Mechanical Engineering at IIT Jodhpur.\n\n'
                         'The Department of Mechanical Engineering at IIT Jodhpur is devoted to quality engineering education and pursuits the excellence in research. The vision of the Department is to translate its research and to innovate in pedagogy to enable ignited young minds to find technological solutions for upcoming challenges.',
-                        style: TextStyle(fontSize: 20),
+                        style: TextStyle(fontSize: 20, color: Colors.white), // Change the color of the text to white
                       ),
                     ],
                   ),
